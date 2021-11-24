3e91d249c92578cfe15112365f5888b99220f8bd
cllzkynk@gmail.com <cllzkynk@gmail.com>
2021-11-24 18:29:52 +0200

    _

--- src/day09_Arrays/Q18.java
+++ src/day09_Arrays/Q18.java
@@ -13,52 +13,62 @@ public class Q18 {
 
 		Scanner scan = new Scanner(System.in);
 		System.out.println("bisiler gir");
-		String str = scan.nextLine().toLowerCase();
+		String str = scan.nextLine();
+		
+		//String str="Javacilar cook afilli";
 		String arr[] = str.split("");
-		Arrays.sort(arr);
-		System.out.println(Arrays.toString(arr));
-		harfsay(arr);
-		tekle(arr);
+		//Arrays.sort(arr);
+		//System.out.println(Arrays.toString(arr));
 		
-		
-		
-		
-	
-System.out.println(tekle(arr));
-//System.out.println(harfsay(arr[i]));
-	}
+		//tekle(arr);
+String tek=tekle(arr);
+String tekliarr[]=tek.split("");
 
+//System.out.println(Arrays.toString(tekliarr));	
+		
+String nihayi = adetSay(tekliarr,arr);	
+//System.out.println(adetSay(tekliarr,arr));
+
+String nihayet[]=new String [tek.length()];
+
+for (int i = 0; i < nihayet.length; i++) {
+	nihayet[i]=tekliarr[i]+"="+nihayi.charAt(i);
+}
+
+System.out.println(Arrays.toString(nihayet));
+
+scan.close();
+
+	}
+	public static String adetSay(String tekliarr[], String[] arr) {
+		String sayici="";
+		int sayac=0;
+		for (int i = 0; i <tekliarr.length; i++) {
+			sayac=0;
+			for (int j = 0; j <arr.length; j++) {
+				if(tekliarr[i].equals(arr[j])) {
+					sayac++;
+					
+				}
+				
+			}
+			sayici+=sayac+"";
+			//System.out.println(tekliarr[i]+"====>"+sayici);
+		}
+		
+		
+		
+		
+	return sayici;	
+	}
 	public static String tekle(String[] arr) {
-		String str = "";
+		String tekli="";
 		for (int i = 0; i < arr.length; i++) {
-
-			for (int j = 0; j < arr.length; j++) {
-
-				if (arr[i].charAt(0) != arr[j].charAt(0) && i != j) {
-					str += arr[i];
-					break;
-				}
-				;
-			}
-
-		}
-		return str;
-	}
-
-	public static int harfsay(String[] arr) {
-		int count = 0;
-		for (int i = 0; i < arr.length; i++) {
-count=0;
-			for (int j = 0; j < arr.length; j++) {
-
-				if (arr[i].charAt(0) == arr[j].charAt(0) && i != j) {
-					count++;
-				}
-				;
+			if (!tekli.contains(arr[i])) {
+				tekli+=arr[i];
 			}
 		}
-
-		return count;
+	return tekli;	
 	}
 
 }
